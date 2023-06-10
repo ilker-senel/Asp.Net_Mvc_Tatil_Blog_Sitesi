a88f5f1c6dcd141698871c88440171acb31fdac9
illkkeerr <ilkersenel5797@gmail.com>
2023-06-10 10:00:28 +0300

    Asp.Net_Mvc_Tatil_Blog_Sitesi

---
+++ Asp.net_Tatil_Sitesi/TravelTripProje/TravelTripProje/Models/Sınıflar/AnaSayfa.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class AnaSayfa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+
+    }
+}
---
+++ Asp.net_Tatil_Sitesi/TravelTripProje/TravelTripProje/Models/Sınıflar/Yorumlar.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class Yorumlar
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Mail { get; set; }
+        public string Yorum { get; set; }
+        public int BlogId { get; set; }
+        public bool Onay { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
